dce21bfc1e405d3402c68a520c9f092457ded8be
Ivan <carlosivanriveraespilco@gmail.com>
2022-07-21 17:48:20 -0500

    termine variables globales

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'naranjaB':   '#FBC63E',
+      'verdeC': '#7BB037',
+      'rojoC':  '#FF4236',
+      'marronC':  '#43311F',
+      'amarilloC':  '#FFC107',
+      'naranjaC':  '#FF7B01',
+    },
   },
   plugins: [],
 }
