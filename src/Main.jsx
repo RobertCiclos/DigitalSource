8d3ad209f523dc54c099cdee322498ad5aa5390f
Robert <robertquillaslopez@gmail.com>
2022-07-21 09:34:05 -0500

    estructura del proyecto y taildwind instalado

---
+++ src/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
+const Main = () => {
+  return(
+    <div 
+      key="Principal">
+      <Navbar />
+      <Footer /> 
+    </div>
+  )
+}
+
+export default Main
