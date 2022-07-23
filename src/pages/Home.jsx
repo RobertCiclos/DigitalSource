a2f41ee7c1b5a1225d01826006075b14a2fe1108
Ivan <carlosivanriveraespilco@gmail.com>
2022-07-22 19:08:14 -0500

    termine request final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,36 @@ import Question1 from '../components/svg/question/Question1'
 import Question2 from '../components/svg/question/Question2'
 import Question3 from '../components/svg/question/Question3'
 import Question4 from '../components/svg/question/Question4'
-
+import BtnRedEmpezar from '../components/buttons/BtnRedEmpezar'
 const Home = () => {
   return (
   <div 
     className=''>
     Home
-
+    <section>
+        <div 
+          className='mx-auto relative
+          w-[414px] h-[284px]
+          md:w-[768px] md:h-[380px]
+          lg:w-[1440px] lg:h-[635px]'>
+          <img src="./imagenes/fondohero.png"
+            className='w-[414px] h-[284px] mx-auto
+            md:w-[768px] md:h-[380px]
+            lg:w-[1440px] lg:h-[635px]'/>
+          <h1
+            className='absolute top-[65px] right-[78.15px]
+            md:top-[90px] md:right-[159px]
+            lg:top-[142px] lg:right-[255px]'>
+            ¡A mangiare con nuestras recetas! 
+          </h1>
+          <div 
+            className="absolute top-[159px] right-[112.5px]
+            md:top-[245px] md:right-[289.5px]
+            lg:top-[387px] lg:right-[550px]">
+            <BtnRedEmpezar />
+          </div>
+        </div>
+    </section>
     <section 
       className='w-full h-[730px] flex items-center mx-auto bg-naranjaB 
       aux:h-[645px]'>
