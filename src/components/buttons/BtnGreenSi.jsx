569e5b02814560b93c3363dfc34c5fc80693b19a
natvvgls <natalia.d.m975@gmail.com>
2022-08-10 13:29:13 -0500

    Proceso de compra 2 terminado

---
+++ src/components/buttons/BtnGreenSi.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BtnGreenSi = (props) => {
+  return (
+    <button
+        className='border-[2px] border-[#7BB037] rounded-[10px] text-center justify-center font-[Poppins] font-bold 
+        text-[16px] w-[144px] h-[49px]
+        lg:text-[20px] lg:w-[295px] lg:h-[70px]'>
+        Si
+    </button>
+  )
+}
+
+export default BtnGreenSi
